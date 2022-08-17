380225fab36bdb5d74103bc00071b57e813c5a2a
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-08-17 16:57:03 -0300

    projeto iniciado
    Criei  um novo projeto do zero com o Nest;
    Subi uma aplicação Nest em modo desenvolvimento;
    Defini rotas para atender requisições HTTP utilizando os principais verbos (GET, PUT, POST, DELETE).
    Testei a API utilizando o Insomnia.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProdutosController } from './produtos.controler';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProdutosController],
   providers: [AppService],
 })
 export class AppModule {}
