eebb67194eb3952235514bb78a5b0431e65d906b
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-08-18 18:37:17 -0300

    alteração dos arquivos .tp
    Realizei a alteração dos arquivos .tp com refatoração (alterando códigos das rotas e iniciando  a conexão com o banco de dados)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controler';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize/dist/sequelize.module";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { LivrosController } from "./livros.controller";
+import { LivrosService } from "./livros.service";
+import { Livro } from "./livro.model";
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "livraria",
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
---
+++ src/livro.model.ts
@@ -0,0 +1,22 @@
+import { Table, Model, Column, DataType } from "sequelize-typescript";
+
+@Table
+export class Livro extends Model<Livro> {
+  @Column({
+    type: DataType.STRING(60),
+    allowNull: false,
+  })
+  codigo: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  nome: string;
+
+  @Column({
+    type: DataType.DECIMAL(10, 2),
+    allowNull: false,
+  })
+  preco: number;
+}
